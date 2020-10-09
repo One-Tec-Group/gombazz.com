37f1f6440162e0a4b8c409ad9883e87f81dacc9e
Moaaz.Abdallah <mezotitans@gmail.com>
2020-10-09 12:44:27 +0000

    moaaz

---
+++ mvc/logs/log-2020-01-20.php
@@ -0,0 +1,48 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-20 00:00:19 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:01:12 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:01:49 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:01:59 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:03:26 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:03:57 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:04:22 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:05:42 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:12:05 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:18:44 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:19:02 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:19:38 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:21:27 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:22:19 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:22:56 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:23:59 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:24:31 --> Could not find the language line "teacher_registerNO"
+ERROR - 2020-01-20 00:24:31 --> Could not find the language line "teacher_registerNO"
+ERROR - 2020-01-20 00:24:31 --> Could not find the language line "teacher_registerNO"
+ERROR - 2020-01-20 00:24:53 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:25:12 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:25:20 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:25:29 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:25:45 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:26:37 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:27:05 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:27:23 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:28:14 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:29:11 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:30:07 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:31:30 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:32:05 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:34:06 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:34:36 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:35:10 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:35:29 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:36:07 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:36:33 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:38:34 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:38:53 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 00:39:57 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 10:31:50 --> Could not find the language line "teacher_registerNO"
+ERROR - 2020-01-20 10:31:50 --> Could not find the language line "teacher_registerNO"
+ERROR - 2020-01-20 10:31:50 --> Could not find the language line "teacher_registerNO"
+ERROR - 2020-01-20 13:46:17 --> 404 Page Not Found: Assets/datatables
+ERROR - 2020-01-20 13:47:13 --> 404 Page Not Found: Assets/datatables
